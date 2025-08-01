074267373ae5ffb251d2a7bc14eba94d0962e092
Mohammad Bilal <M.bilal14241@gmail.com>
2025-08-01 22:54:11 +0500

    iris Predictor.py

--- iris Predictor.py
+++ iris Predictor.py
@@ -35,7 +35,7 @@ df['class_encoded'] = encoder.fit_transform(df['class'])
 # Splitting the dataset into training and testing sets
 X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
 Y = df['class_encoded']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=42)
 
 # Training the KNN model
 KNN = KNeighborsClassifier(n_neighbors = 3)
